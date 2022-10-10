0096cbc300ab92ba00f2a4c0a5c3f94b8c13d738
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2022-10-10 17:19:35 +0200

    update

---
+++ TP1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+int nb_parfait(int N){
+    for (int i = 2; i<=N; i++){
+        int s=0;
+        for (int j = 1; j<i/2+1; j++){
+            if (i%j==0){
+                s+=j;
+            }
+        }
+        if (s==i){
+            printf("%d est parfait \n",i);
+        }
+    }
+    return 0;
+}
+
+int main(void) {
+    int i;
+    printf("Saisissez le nombre maximal :\n");
+    scanf("%d", &i);
+    nb_parfait(i);
+    return EXIT_SUCCESS;
+}
