6f17c96a8096beba39ec023249ba61f0824c350a
Marc LG <mega.stunt49@gmail.com>
2022-10-12 11:23:51 +0200

    TP3 Initial Commit

---
+++ TP-03-PrixTuring/prixTuring.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+char* scanLine(){
+	int maxLineSize = 255;
+	char c, *line = calloc(maxLineSize+1,sizeof(char));
+
+	scanf("%250[^\n]", line);
+
+	if ( (c = getchar()) != '\n'){
+		line[250] = '[';
+		line[251] = line[252] = line[253] = '.';
+		line[254] = ']';
+		while (( (c = getchar()) != '\n') && c != EOF) ;
+	}
+
+	return line;
+}
+
+int scanLineAsInt(){
+	int buf;
+	scanf("%i\n",&buf);
+	return buf;
+}
+
+
+int main(void){
+	int nbGagnants = scanLineAsInt();
+	printf("nbGagnants = %i\n",nbGagnants);
+	int yearFirstWinner = scanLineAsInt();
+	printf("yearFirstWinner = %i\n",yearFirstWinner);
+	char *nameFirstWinner = scanLine();
+	printf("nameFirstWinner = %i\n",nameFirstWinner);
+	return EXIT_SUCCESS;
+}
