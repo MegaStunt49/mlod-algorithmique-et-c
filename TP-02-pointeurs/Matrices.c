8a2396f7177b8cdf83570860b93fcc6dadd5a316
Marc LG <mega.stunt49@gmail.com>
2022-10-12 08:30:56 +0200

    Fin Programme Matrice + début dates

--- TP-02-pointeurs/Matrices.c
+++ TP-02-pointeurs/Matrices.c
@@ -3,11 +3,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-void matrix_mult(int64_t mR[][],int64_t m1[][],int64_t m2[][]){
-    for (int i = 0; i<sizeof(m1); i++){
-        for (int j = 0; j<sizeof(m1); j++){
+#define SIZE  5
+
+void matrix_mult(int64_t mR[SIZE][SIZE],int64_t m1[SIZE][SIZE],int64_t m2[SIZE][SIZE]){
+    for (int i = 0; i<SIZE; i++){
+        for (int j = 0; j<SIZE; j++){
             int s = 0;
-            for (int k = 0; k<sizeof(m1);k++){
+            for (int k = 0; k<SIZE;k++){
                 s+=m1[i][k]*m2[k][j];
             }
             mR[i][j]=s;
@@ -15,7 +17,20 @@ void matrix_mult(int64_t mR[][],int64_t m1[][],int64_t m2[][]){
     }
 }
 
+void matrix_print(int64_t m[SIZE][SIZE]){
+    for (int i = 0; i<SIZE; i++){
+        for (int j = 0; j<SIZE; j++){
+            printf(" %ld ",m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main(void) {
-    ;
+    int64_t ma1[SIZE][SIZE]={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
+    int64_t ma2[SIZE][SIZE]={{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10}};
+    int64_t mar[SIZE][SIZE];
+    matrix_mult(mar,ma1,ma2);
+    matrix_print(mar);
     return EXIT_SUCCESS;
 }
---
+++ TP-02-pointeurs/dates.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef enum {Janvier,Fevrier,Mars,Avril,Mai,Juin,Juillet,Août,Septembre,Octobre,Novembre,Décembre} Mois;
+
