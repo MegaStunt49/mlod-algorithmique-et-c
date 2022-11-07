9e270ab43a28c4c1f9ec10a569ab8db5e10ad0b8
Marc LG <mega.stunt49@gmail.com>
2022-11-07 14:46:43 +0100

    Final Commit

--- TP-Note/tpnote1.c
+++ TP-Note/tpnote1.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "tpnote1.h"
 
 // Le Goff Marc
 
@@ -10,6 +12,7 @@ Tabl repeat(Tabl t1, Tabl t2){
         s+=t1.valeurs[i];
     }
     val3 = calloc(s,sizeof(int));
+    t3.size = s;
     t3.valeurs = val3;
     int s1 = 0;
     for (int i = 0; i<t1.size; i++){
@@ -23,6 +26,18 @@ Tabl repeat(Tabl t1, Tabl t2){
 
 
 int main(void) {
-    ;
+    Tabl t1;
+    t1.size = 3;
+    int val1[3] = {1,2,4};
+    t1.valeurs = val1;
+    Tabl t2;
+    t2.size = 3;
+    int val2[3] = {10,3,8};
+    t2.valeurs = val2;
+    Tabl t3 = repeat(t1,t2);
+    for (int j = 0; j<t3.size; j++){
+            printf(" %d ",t3.valeurs[j]);
+        }
+        printf("\n");
     return EXIT_SUCCESS;
 }
